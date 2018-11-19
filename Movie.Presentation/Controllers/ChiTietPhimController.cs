99f4120ce2a1fed86f1ea3a0e4d4c958a389d237
soncat1 <siunhan1212@gmail.com>
2018-11-19 16:44:07 +0700

    Sửa layout

---
+++ Movie.Presentation/Controllers/ChiTietPhimController.cs
@@ -0,0 +1,23 @@
+﻿using Movie.Service;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace Movie.Presentation.Controllers
+{
+    public class ChiTietPhimController : Controller
+    {
+        private FilmService filmService;
+        public ChiTietPhimController()
+        {
+            filmService = new FilmService();
+        }
+        // GET: ChiTietPhim
+        public ActionResult Index(int id)
+        {
+            return View(filmService.GetFilm(id));
+        }
+    }
+}
